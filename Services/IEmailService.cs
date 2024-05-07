2db9e9d56bed7cf5919ff9e0bb6647813247b7a3
Edvin <edvin.thornholm@gmail.com>
2024-05-07 13:46:05 +0200

    Add project files.

---
+++ Services/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Messaging.ServiceBus;
+using EmailProviderNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailProviderNew.Services;
+
+public interface IEmailService
+{
+    bool SendEmail(EmailRequest emailRequest);
+    EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message);
+}
